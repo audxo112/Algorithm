bd007fc48fcd261c32c2a74e20dacb3f9f666efd
HOME <audxo112@gmail.com>
2022-06-03 22:55:50 +0900

    Modify BFS 7569

--- 0x09/bfs/7569_tomato.py
+++ 0x09/bfs/7569_tomato.py
@@ -1,44 +1,35 @@
 # https://www.acmicpc.net/problem/7569
 import sys
-from collections import deque
 
 
 dirs = [(0, 1, 0), (-1, 0, 0), (0, -1, 0), (1, 0, 0), (0, 0, 1), (0, 0, -1)]
 
 
-def bfs(M, N, H, board, queue):
-    cur = -1
+def solution(M, N, H, board):
+    queue, zero, date = [], 0, -1
+    for z in range(H):
+        for y in range(N):
+            for x in range(M):
+                if board[z][y][x] == "0":
+                    zero += 1
+                elif board[z][y][x] == "1":
+                    queue.append((x, y, z))
     while queue:
-        x, y, z, cnt = queue.popleft()
-        cur = cnt
-        board[z * M * N + y * M + x] = -1
-
-        for (dx, dy, dz) in dirs:
-            nx, ny, nz = x + dx, y + dy, z + dz
-            if nx < 0 or nx >= M or ny < 0 or ny >= N or nz < 0 or nz >= H or board[nz * M * N + ny * M + nx]:
-                continue
-            board[nz * M * N + ny * M + nx] = -1
-            queue.append((nx, ny, nz, cnt + 1))
-    return cur
-
-
-def solution(M, N, H, board, queue):
-    result = bfs(M, N, H, board, queue)
-    if sum(board) == - M * N * H:
-        return result
-    else:
-        return -1
+        date += 1
+        tmp = []
+        for x, y, z in queue:
+            for (dx, dy, dz) in dirs:
+                nx, ny, nz = x + dx, y + dy, z + dz
+                if nx < 0 or nx >= M or ny < 0 or ny >= N or nz < 0 or nz >= H or \
+                        board[nz][ny][nx] != "0":
+                    continue
+                board[nz][ny][nx] = "1"
+                tmp.append((nx, ny, nz))
+        zero -= len(tmp)
+        queue = tmp
+    return -1 if zero else date
 
 
 IM, IN, IH = map(int, sys.stdin.readline().split())
-IBoard = []
-IQueue = deque()
-for z in range(IH):
-    for y in range(IN):
-        for x, t in enumerate(map(int, sys.stdin.readline().split())):
-            if t == 1:
-                IQueue.append((x, y, z, 0))
-            IBoard.append(t)
-
-print(solution(IM, IN, IH, IBoard, IQueue))
-
+IBoard = [[sys.stdin.readline().split() for _ in range(IN)] for _ in range(IH)]
+print(solution(IM, IN, IH, IBoard))
