ea7e36ae4da4c0d0534949515fb96571dd1cec66
HOME <audxo112@naver.com>
2022-07-06 01:57:29 +0900

    Comment BFS 17071

--- 0x09/bfs/17071_hide_and_seek5.py
+++ 0x09/bfs/17071_hide_and_seek5.py
@@ -1,4 +1,4 @@
-# https://www.acmicpc.net/problem/7569
+# https://www.acmicpc.net/problem/17071
 import sys
 from collections import deque
 
@@ -6,6 +6,12 @@ from collections import deque
 def solution(N, K):
     if N == K:
         return 0
+
+    # 수빈이가 방문한 지점을 저장을 하는데 홀수 시간 인지 짝수 시간 인지를 저장 한다
+    # 수빈이가 +1 -1을 각각 1번씩 실행하게 되면 같은 위치게 오게되고
+    # 동생이 그 지점을 밟게 되면 동생과 수빈이가 만나는 시간이된다
+    # visited 의 경우 수빈이가 밟을때 저장을 하기 때문에
+    # 무조건 동생이 밟은 시점보다 이전이다
 
     visited = [[False] * 500001 for _ in range(2)]
     visited[0][N] = True
@@ -25,6 +31,7 @@ def solution(N, K):
                     continue
                 if nn == K:
                     return t
+                # 지난 시간기준 짝, 홀을 고려하여 밟은적 있는지를 저장한다
                 visited[t % 2][nn] = True
                 queue.append(nn)
     return -1
---
+++ 0x0D/simulation/17140_2d_array_and_operations.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/17140
+import sys
+
+
+def solution(R, C, K, array):
+    pass
+
+
+IR, IC, IK = map(int, sys.stdin.readline().split())
+IArray = [list(map(int, sys.stdin.readline().split())) for _ in range(3)]
+print(solution(IR, IC, IK, IArray))
