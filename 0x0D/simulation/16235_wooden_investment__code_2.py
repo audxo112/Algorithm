a1d397a25d5d4318da80819f9e106fb4f04e3869
HOME <audxo112@naver.com>
2022-07-05 12:14:13 +0900

    Solve simulation 16236

--- 0x0D/simulation/16235_wooden_investment__code_2.py
+++ 0x0D/simulation/16235_wooden_investment__code_2.py
@@ -6,6 +6,50 @@ import sys
 dirs = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
 
 
+def simulation(N, A, foods, trees):
+    breading = dict()
+    for y in range(N):
+        for x in range(N):
+            if not trees[y][x]:
+                foods[y][x] += A[y][x]
+                continue
+            bread = death = 0
+            exist = True
+            nxt = dict()
+            for age, count in sorted(trees[y][x].items()):
+                if exist:
+                    num = foods[y][x] // age
+                    if num >= count:
+                        foods[y][x] -= age * count
+                        nxt[age + 1] = count
+                        if not (age + 1) % 5:
+                            bread += count
+                    else:
+                        if num:
+                            foods[y][x] -= age * num
+                            nxt[age + 1] = num
+                            if not (age + 1) % 5:
+                                bread += num
+                        exist = False
+                        death += (age >> 1) * (count - num)
+                else:
+                    death += (age >> 1) * count
+            trees[y][x] = nxt
+            foods[y][x] += death + A[y][x]
+            if bread:
+                for (dx, dy) in dirs:
+                    nx, ny = x + dx, y + dy
+                    if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                        continue
+                    if (nx, ny) not in breading:
+                        breading[(nx, ny)] = bread
+                    else:
+                        breading[(nx, ny)] += bread
+
+    for (x, y), cnt in breading.items():
+        trees[y][x][1] = cnt
+
+
 def solution(N, M, K, A, info):
     foods = [[5] * N for _ in range(N)]
     trees = [[{} for _ in range(N)] for _ in range(N)]
@@ -14,47 +58,7 @@ def solution(N, M, K, A, info):
         trees[y - 1][x - 1][age] = 1
 
     for k in range(K):
-        breading = dict()
-        for y in range(N):
-            for x in range(N):
-                if not trees[y][x]:
-                    foods[y][x] += A[y][x]
-                    continue
-                bread = death = 0
-                exist = True
-                nxt = dict()
-                for age, count in sorted(trees[y][x].items()):
-                    if exist:
-                        num = foods[y][x] // age
-                        if num >= count:
-                            foods[y][x] -= age * count
-                            nxt[age + 1] = count
-                            if not (age + 1) % 5:
-                                bread += count
-                        else:
-                            if num:
-                                foods[y][x] -= age * num
-                                nxt[age + 1] = num
-                                if not (age + 1) % 5:
-                                    bread += num
-                            exist = False
-                            death += (age >> 1) * (count - num)
-                    else:
-                        death += (age >> 1) * count
-                trees[y][x] = nxt
-                foods[y][x] += death + A[y][x]
-                if bread:
-                    for (dx, dy) in dirs:
-                        nx, ny = x + dx, y + dy
-                        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-                            continue
-                        if (nx, ny) not in breading:
-                            breading[(nx, ny)] = bread
-                        else:
-                            breading[(nx, ny)] += bread
-
-        for (x, y), cnt in breading.items():
-            trees[y][x][1] = cnt
+        simulation(N, A, foods, trees)
 
     return sum([sum(trees[y][x].values()) for x in range(N) for y in range(N)])
 
--- 0x0D/simulation/16236_baby_shark.py
+++ 0x0D/simulation/16236_baby_shark.py
@@ -1,1 +1,60 @@
 # https://www.acmicpc.net/problem/16236
+import sys
+
+
+dirs = [(0, -1), (-1, 0), (1, 0), (0, 1)]
+
+
+def simulation(N, board, visited, sx, sy, shark, fish):
+    queue = [(sx, sy)]
+    visited[sy][sx] = fish
+    time = 0
+    while queue:
+        tmp = []
+        for x, y in queue:
+            if board[y][x] and shark > board[y][x]:
+                board[y][x] = 0
+                return time, x, y
+
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if nx < 0 or nx >= N or ny < 0 or ny >= N or visited[ny][nx] == fish or board[ny][nx] > shark:
+                    continue
+
+                visited[ny][nx] = fish
+                tmp.append((nx, ny))
+        queue = sorted(tmp, key=lambda p: (p[1], p[0]))
+        time += 1
+
+    return 0, sx, sy
+
+
+def solution(N, board):
+    sx = sy = fish = 0
+    for y in range(N):
+        for x in range(N):
+            if board[y][x] == 9:
+                sx, sy = x, y
+                board[y][x] = 0
+            elif board[y][x]:
+                fish += 1
+    if fish == 0:
+        return 0
+
+    shark, e_cnt, time = 2, 0, 0
+    visited = [[fish + 1] * N for _ in range(N)]
+    while fish:
+        t, sx, sy = simulation(N, board, visited, sx, sy, shark, fish)
+        if not t:
+            return time
+
+        time, fish, e_cnt = time + t, fish - 1, e_cnt + 1
+        if e_cnt == shark:
+            shark += 1
+            e_cnt = 0
+    return time
+
+
+IN = int(sys.stdin.readline())
+IBoard = [list(map(int, sys.stdin.readline().split())) for _ in range(IN)]
+print(solution(IN, IBoard))
